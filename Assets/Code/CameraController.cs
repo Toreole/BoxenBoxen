11ed61e7d691294705b513d59f378c821abd07d1
Toreole <toreole.official@gmail.com>
2019-04-08 13:18:54 +0200

    Camera movement stuff

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    protected float minDistance;
+
+    [SerializeField]
+    protected Transform boxA;
+    [SerializeField]
+    protected Transform boxB;
+
+    [SerializeField]
+    protected Transform centre;
+
+    protected float distance;
+
+    protected Vector3 MeanPosition => Vector3.Lerp(boxA.position, boxB.position, 0.5f);
+
+    private void Start()
+    {
+        distance = Mathf.Clamp(Vector3.Distance(centre.position, transform.position), minDistance, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        centre.position = MeanPosition;
+        centre.forward = (boxA.position - boxB.position).normalized;
+
+        distance = Mathf.Clamp(Vector3.Distance(boxA.position, boxB.position), minDistance, 100);
+
+        transform.position = centre.position - transform.forward * distance;
+    }
+}
---
+++ Assets/Code/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform target;
+    
+    void Update()
+    {
+        var tempPos = target.position;
+        tempPos.y = transform.position.y;
+        transform.forward = (tempPos - transform.position).normalized;
+    }
+}
